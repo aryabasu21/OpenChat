93d32a148e5ee22d38265f8dd42967658b9e4cd2
Arya Basu <basu.arya007@gmail.com>
2025-08-09 12:19:30 +0530

    for uptime

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,8 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '10mb' })); // Increase the limit
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "100mb" })); // Increase the limit
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.use(cookieParser());
 app.use(
@@ -38,7 +38,12 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Simple health check endpoint
+app.get("/api/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
-  connectDB()
+  connectDB();
 });
