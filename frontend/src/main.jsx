fce523a752baec4a503295f80fcaadf239b060ea
Arya Basu <basu.arya007@gmail.com>
2025-10-27 14:00:41 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
     future: {
       v7_relativeSplatPath: true, // ✅ removes warning
       v7_startTransition: true,
-      v7_fetchPersist: true,
+      v7_fetcherPersist: true,
       v7_normalizeFormMethod: true,
       v7_partialHydration: true,
       v7_skipActionErrorRevalidation: true,
