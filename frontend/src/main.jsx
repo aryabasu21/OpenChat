39204732c33c7606c22e031a7813eab5271c500c
Arya Basu <basu.arya007@gmail.com>
2025-10-27 13:07:06 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*", // ✅ all routes handled by App.jsx
+      element: <App />,
+    },
+  ],
+  {
+    future: {
+      v7_relativeSplatPath: true, // ✅ removes warning
+    },
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
